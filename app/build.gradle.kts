b166ce0c395219e1f72e150721ae16eadc09ab7a
Alexey Kurnakov <kurnakov.a.v@mailru>
2025-11-24 21:20:26 +0300

    Initial clean project commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    // Плагин для Java
+    id("java")
+
+    // Плагин для запуска приложения
+    id("application")
+
+    // Плагин, который проверяет новые версии зависимостей
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    // Главный класс приложения
+    mainClass.set("hexlet.code.App")
+}
+
+// (опционально) Настройки Java версии
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}

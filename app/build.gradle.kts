cdb04fbae5b7a30fd4fdcf41e81332517383d97d
Alexey Kurnakov <kurnakov.a.v@mailru>
2025-11-27 22:09:41 +0300

    add Checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,31 +1,25 @@
 plugins {
-    // Плагин для Java
-    id("java")
-
-    // Плагин для запуска приложения
-    id("application")
-
-    // Плагин, который проверяет новые версии зависимостей
+    application
     id("com.github.ben-manes.versions") version "0.51.0"
-
     id("org.sonarqube") version "7.1.0.6387"
+    checkstyle
 }
+
 sonar {
     properties {
         property("sonar.projectKey", "AlekseyKurnakov_java-project-61")
         property("sonar.organization", "alekseykurnakov")
     }
 }
+
 repositories {
     mavenCentral()
 }
 
 application {
-    // Главный класс приложения
     mainClass.set("hexlet.code.App")
 }
 
-// (опционально) Настройки Java версии
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
@@ -34,5 +28,11 @@ java {
 
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
+}
+
+checkstyle {
+    toolVersion = "10.17.0"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
 }
 
